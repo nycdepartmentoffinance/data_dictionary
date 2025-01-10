b5f9ec5bd647cbe21b51b5463fc8c2ec92066d32
Claire Boyd <BoydClaire@finance.nyc.gov>
2025-01-10 17:01:48 -0500

    some of the crosswalk logic

--- generate_table_list.R
+++ generate_table_list.R
@@ -29,6 +29,20 @@ query <-
 
 production_columns <- dbGetQuery(con, query)
 
+query = glue::glue("
+    SELECT *
+    FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+    WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + QUOTENAME(CONSTRAINT_NAME)), 'IsPrimaryKey') = 1
+")
+
+all_production_primary_keys <- dbGetQuery(con, query)
+
+production_primary_keys = all_primary_keys %>%
+    filter(!grepl("_MNC",COLUMN_NAME)) %>%
+    group_by(TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME) %>%
+    arrange(TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, ORDINAL_POSITION) %>%
+    summarize(PRIMARY_KEYS = stringr::str_c(COLUMN_NAME, collapse = ", "), .groups = "drop")
+
 production_tables = production_columns %>%
     group_by(TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME) %>%
     summarise(COLUMNS = n(),
@@ -37,7 +51,8 @@ production_tables = production_columns %>%
     select(TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMNS, COLUMN_NAMES) %>%
     mutate(first_col = str_extract(COLUMN_NAMES, "^[^,]+"),
            PREFIX = ifelse(grepl("_", first_col),
-                           sub("^(.*?)_.*$", "\\1", first_col), NA))
+                           sub("^(.*?)_.*$", "\\1", first_col), NA)) %>%
+    left_join(production_primary_keys, by=c("TABLE_CATALOG", "TABLE_SCHEMA", "TABLE_NAME"))
 
 DBI::dbDisconnect(con)
 
@@ -118,6 +133,8 @@ ias_tables = ias_columns %>%
     mutate(PREFIX = NA,
            first_col = NA)
 
+DBI::dbDisconnect(con)
+
 
 # BIND ALL TABLES TOGETHER
 
@@ -138,21 +155,28 @@ table_list <- all_tables %>%
            !(SCHEMA %in% c("dbo", "COMMON", "internal", "DATA_BACKUP", "DataBackup", "CONV", "NYC_STAGE"))) %>%
     select(DATABASE, SCHEMA, TABLE_NAME, PREFIX, COLS, COLUMN_NAMES, NOTES)
 
-
-
-
-
-
-
-
-
-
-
-
-
 write.csv(table_list, "table_list.csv", na="")
 
 
+# match production // fdw tables
+real_prop = production_tables %>%
+    filter(TABLE_SCHEMA == "REAL_PROP")
+
+fdw_crosswalk = fdw_tables %>%
+    filter(grepl("VW_CAMA_", TABLE_NAME)) %>%
+    mutate(CAMA_NAME = sub(".*VW_CAMA_", "", TABLE_NAME),
+           HIST_CAMA_NAME = ifelse(grepl("_HIST", TABLE_NAME),
+                                   sub("_HIST", "", sub(".*VW_CAMA_", "", TABLE_NAME)), NA)) %>%
+    rename(FDW_NAME = TABLE_NAME) %>%
+    select(FDW_NAME, CAMA_NAME, HIST_CAMA_NAME, COLUMNS) %>%
+    # EXCEPTIONS FOR JOINING
+    mutate(CAMA_NAME = ifelse(CAMA_NAME == "PARCEL_EXTENSIONS", "PARCEL_EXTENSION", CAMA_NAME)) %>%
+    left_join(real_prop, by=c("CAMA_NAME"="TABLE_NAME")) # %>%
+    #left_join(real_prop, by=c("HIST_CAMA_NAME"="TABLE_NAME"))
+
+
+fdw_crosswalk = fdw_tables %>%
+    filter(grepl("VW_CAMA_", TABLE_NAME) & grepl("_HIST", TABLE_NAME))
 
 
 
@@ -160,22 +184,17 @@ write.csv(table_list, "table_list.csv", na="")
 
 
 
+query <-
+    glue::glue("
+    SELECT *
+      FROM FDW3NF.VW_CAMA_REALMAST
+      WHERE ROWNUM <= 100
+        ;
+    ")
 
+bldg <- dbGetQuery(con, query)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+hist <-dbGetQuery(con, query)
 
 
 
